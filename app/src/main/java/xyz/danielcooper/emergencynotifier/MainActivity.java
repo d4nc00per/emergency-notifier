214f9bae976335bcd9afd8a8921bd56c30b25b57
Daniel Cooper <cooper.daniel45@gmail.com>
2016-01-03 22:44:52 +0000

    Added click handler for the send button

--- app/src/main/java/xyz/danielcooper/emergencynotifier/MainActivity.java
+++ app/src/main/java/xyz/danielcooper/emergencynotifier/MainActivity.java
@@ -1,9 +1,18 @@
 package xyz.danielcooper.emergencynotifier;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.SmsManager;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +27,47 @@ public class MainActivity extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.main_menu, menu);
         return true;
+    }
+
+    public void sendButtonClicked(View v) {
+        assert(v.getId() == R.id.sendButton);
+
+        // get saved contacts
+        String[] numbers = new String[] { "07711120889" };
+
+        // get saved message
+        String msg = "Hello World";
+
+
+        // send message
+        String SMS_SENT = "SMS_SENT";
+
+        PendingIntent sentPendingIntent = PendingIntent.getBroadcast(this, 0, new Intent(SMS_SENT), 0);
+
+        // For when the SMS has been sent
+        registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(context, "SMS sent successfully", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(context, "Generic failure cause", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(context, "Service is currently unavailable", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(context, "No pdu provided", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(context, "Radio was explicitly turned off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SMS_SENT));
+
+        SmsService.SendMessage(msg, numbers, sentPendingIntent);
     }
 }
